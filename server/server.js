82307efde93b28585f631f3153c33aaa61a1880f
Anthesk <toine.escourrou@gmail.com>
2022-09-06 23:17:35 +0200

    added files

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import bp from 'body-parser'
 import addScript from './post/create_script.js'
-import listScripts from './get/get_script.js'
+import addShot from './post/create_shot.js'
+import listScripts from './get/get_scripts.js'
 import listShots from './get/get_shots.js'
 
 const app = express()
@@ -15,6 +16,7 @@ app.get('/listShots', listShots)
 
 // post
 app.post('/addScript', addScript)
+app.post('/addShot', addShot)
 
 const server = app.listen(3000, function () {
   const host = server.address().address
