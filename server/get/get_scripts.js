cc3121587588b7e6f5b47debc8e65a69f137fc6d
Anthesk <toine.escourrou@gmail.com>
2022-09-07 20:52:54 +0200

    added getScripts

--- server/get/get_scripts.js
+++ server/get/get_scripts.js
@@ -1,5 +1,14 @@
+import { ObjectId } from 'mongodb'
 import Mongo from '../mongo.js'
 
 export default async function (req, res) {
-  res.send(await Mongo.collection('scripts').find({}).toArray())
+  let response
+
+  if (req.query._id != null) {
+    response = await Mongo.collection('scripts').find(ObjectId(req.query._id)).toArray()
+  } else {
+    response = await Mongo.collection('scripts').find({}).toArray()
+  }
+
+  res.send(response)
 }
