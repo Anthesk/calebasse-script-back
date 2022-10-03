be4469062c0c2193822324013eb2a69d0edefa72
Anthesk <toine.escourrou@gmail.com>
2022-10-03 18:51:32 +0200

    security second update

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
+const mongoUser = 'calebasse-script-back';
+const mongoPwd = '+Pf}iu.%5B2xG2b8VQ=Dr';
+
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:26083',
+  mongoUrl: `mongodb://${mongoUser}:${mongoPwd}@mongo:26083`,
   dbName: 'calebasse-script-back'
 }
